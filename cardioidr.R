d11e03ba499ebcfc7eec396dac06a7e4483da474
Aaron Ferrucci <aaronf@antfarm.org>
2015-11-29 14:09:58 -0800

    Omit grid, axes.

--- cardioidr.R
+++ cardioidr.R
@@ -11,9 +11,24 @@ cardioid <- function(num_pts, stride) {
   end_indices[which(end_indices == 0)] = num_pts
   endx <- pts$x[end_indices]
   endy <- pts$y[end_indices]
-  df2 <- data.frame(x = startx, xend= endx, y = starty, yend = endy)
+  lines <- data.frame(x = startx, xend= endx, y = starty, yend = endy)
   p <- ggplot(pts, aes(x = x, y = y)) + geom_point()
-  p <- p + geom_segment(data=df2, mapping=aes(x=x, y=y, xend=xend,yend=yend))
+  p <- p + geom_segment(data=lines, mapping=aes(x=x, y=y, xend=xend,yend=yend))
+  # ggplot can be convinced to omit axes and grid lines.
+  p <- p + theme(
+    axis.line=element_blank(),
+    axis.text.x=element_blank(),
+    axis.text.y=element_blank(),
+    axis.ticks=element_blank(),
+    axis.title.x=element_blank(),
+    axis.title.y=element_blank(),
+    legend.position="none",
+    panel.background=element_blank(),
+    panel.border=element_blank(),
+    panel.grid.major=element_blank(),
+    panel.grid.minor=element_blank(),plot.background=element_blank()
+  )
+  
   return(p)
 }
 
